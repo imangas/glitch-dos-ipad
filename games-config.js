7fedc72820bd814336f987664a2e7687baf92ce9
NachoMangas <ignacio.mangas@serviceclub.com>
2026-08-15 20:33:17 +0200

    feat: add file with default config for multiple games

---
+++ games-config.js
@@ -0,0 +1,43 @@
+window.gamesData = {
+  generic: {
+    "name": "generic",
+    "description": "...",
+    "dosSaveFilePath": "SC2K",
+    "dosSaveFileName": "CITY.M2",
+    "opfsFolder": "sc2k",
+    "dosboxConfig": 
+      "[autoexec]\nmount c .\nc:\nSC2000.EXE"
+    
+  },
+  "sc2000": {
+    "name": "SimCity 2000",
+    "description": "Simulador de construcción y gestión de ciudades.",
+    "company": "3DO",
+    "dosSaveFilePath": ".",
+    "dosSaveFileName": "GAME.SC2",
+    "opfsSaveFilePath": "simcity2k  ",
+    "dosboxConfig": 
+      "[autoexec]\nmount c .\nc:\nSC2000.EXE"
+    
+  },
+  "ultima-7": {
+    "name": "Ultima Online 7: The black Gate",
+    "description": "Clásico shooter en primera persona.",
+    "dosSaveFilePath": "DOOM",
+    "dosSaveFileName": "DOOMSAV0.DSG",
+    "opfsFolder": "doom",
+    "dosboxConfig": 
+      "[autoexec]\nmount c .\nc:\ncd ULTIMA~1\nultima7\n[dos]\nxms=true\nems=false"
+    
+  },
+  "doom": {
+    "name": "DOOM",
+    "description": "Clásico shooter en primera persona.",
+    "dosSaveFilePath": "DOOM",
+    "dosSaveFileName": "DOOMSAV0.DSG",
+    "opfsFolder": "doom",
+    "dosboxConfig": 
+      "[autoexec]\nmount c .\nc:"
+    
+  }
+};
